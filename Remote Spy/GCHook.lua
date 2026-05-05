5861abb953eca1499c92bf326a6e63382a7996af
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 22:02:33 -0500

    Create GCHook.lua

---
+++ Remote Spy/GCHook.lua
@@ -0,0 +1,13 @@
+local remote = Instance.new("RemoteEvent")
+local before = gcinfo() 
+
+for i = 1, 100 do
+    remote:FireServer()
+end
+
+--[[ 
+    should always be 0 bc fireserver is a native C function, It doesn't use memory. 
+    
+    If it's > 0, an exploit hooked it w/ a Lua function.
+]]
+print("gc delta on fireserver:", gcinfo() - before)
