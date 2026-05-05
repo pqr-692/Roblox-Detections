9f2dd483ed92258537ca2944c746d19927cb058f
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 21:54:58 -0500

    Create Cobolt.lua

---
+++ Remote Spy/Cobolt.lua
@@ -0,0 +1,26 @@
+-- [[ https://github.com/notpoiu/cobalt ]]
+
+loadstring(game:HttpGet("https://github.com/notpoiu/cobalt/releases/latest/download/Cobalt.luau"))()
+
+if game:GetService("RunService"):IsClient() then
+    task.spawn(function()
+        while true do
+            local BaitRemote = Instance.new("RemoteEvent")
+            
+            --[[  wax.shared.Hooking.HookMetaMethod(game, "__newindex", function(...) ]]
+            local _, err = pcall(function()
+                BaitRemote[{}] = true
+            end)
+            
+            if err then
+                local line = tonumber(err:match(":(%d+):"))
+                if line and line > 50 then
+                    warn("Detected cobalt")
+                end
+            end
+            
+            BaitRemote:Destroy()
+            task.wait(1)
+        end
+    end)
+end
