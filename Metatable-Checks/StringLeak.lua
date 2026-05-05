4e28fa0ae9eda2ef12ee81c2b9e069d1fa23f4a4
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 22:27:56 -0500

    Update StringLeak.lua

--- Metatable-Checks/StringLeak.lua
+++ Metatable-Checks/StringLeak.lua
@@ -3,6 +3,7 @@ local str = "test"
 while task.wait(1) do
     --[[ 
         catches 'setrawmetatable' abuse. if an executor sets a string's metatable to getgenv(), strings leak the whole environment.
+        ref: https://docs.sunc.su/Metatable/setrawmetatable/
     ]]
     if str.print ~= nil then
         -- finding 'print' means the metatable was hijacked to link to globals.
