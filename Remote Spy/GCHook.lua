17ff1d5fcc0411ae3e325d7e7469fc0803a0fe51
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 22:10:58 -0500

    Update GCHook.lua

--- Remote Spy/GCHook.lua
+++ Remote Spy/GCHook.lua
@@ -8,6 +8,6 @@ end
 --[[ 
     should always be 0 bc fireserver is a native C function, It doesn't use memory. 
     
-    If it's > 0, an exploit hooked it w/ a Lua function.
+    If it's > 0, an exploit hooked it w/ a Lua function. This also should work on any other namecall hook
 ]]
 print("gc delta on fireserver:", gcinfo() - before)
