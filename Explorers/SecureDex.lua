e7f64614c14fcaa69f4f2d3b7a14fb666bf57c38
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 23:03:58 -0500

    Update SecureDex.lua

--- Explorers/SecureDex.lua
+++ Explorers/SecureDex.lua
@@ -1,4 +1,4 @@
--- potassium this method just works on potassium since potassium leaks their pcall stack. you can still detect this with the XpcallPath method
+-- this method most likely just works on potassium since potassium leaks their pcall stack. you can still detect this with the XpcallPath method
 
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
