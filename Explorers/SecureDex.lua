2e7c2d6a88e002b5bbce488cf3b8e1377e8c481b
pqr-692 <47876119+pqr-692@users.noreply.github.com>
2026-05-04 23:02:48 -0500

    Create SecureDex.lua

---
+++ Explorers/SecureDex.lua
@@ -0,0 +1,33 @@
+-- Possible this method just works on potassium since potassium leaks their pcall stack.
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+
+local last = tick()
+
+RunService.Heartbeat:Connect(function()
+    if tick() - last < 3 then return end
+    last = tick()
+
+    local c = 0
+
+    local function f()
+        c = c + 1
+        xpcall(f, function() end)
+    end
+
+    task.spawn(function()
+        pcall(f)
+    end)
+
+    task.wait()
+
+    --[[ 
+        uses stack overflow to catch dex. 
+        dex hooks pcall/xpcall to log errors, which blocks recursion and keeps it at 1.
+    ]]
+    if c == 1 then
+        lp:Kick("Secure Dex")
+    end
+end)
